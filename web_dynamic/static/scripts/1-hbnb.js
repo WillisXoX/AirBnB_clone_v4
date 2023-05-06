8c0ea3df881739a1d8b8000f575b733972d56f4f
Willis <Williamstone4041@gmail.com>
2023-05-06 18:41:28 +0100

    Final commit

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(() => {
 
     var amenities = $('.amenity-box');
@@ -35,22 +34,3 @@ $(document).ready(() => {
         });
     }
 });
-=======
-$( document ).ready(function () {
-
-  /*****************************************************
-    display list of checkboxes clicked
-   *****************************************************/
-  let ls_amen = [];
-  $('input[type=checkbox]').change (function () {
-    let name = $(this).attr('data-name');
-      if ($(this).is(':checked')) {
-	ls_amen.push(name);
-      } else {
-	ls_amen = ls_amen.filter(amen => amen !== name);
-      }
-    $('.amenities h4').text(ls_amen.join(', '));
-  });
-
-});
->>>>>>> 7935defc74ddedb01422bd63848c4da05a9eba93
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(document).ready(() => {
 
     var amenities = $('.amenity-box');
@@ -47,38 +46,3 @@ $(document).ready(() => {
         }
       });
 });
-=======
-$( document ).ready(function () {
-
-  /*****************************************************
-    display list of checkboxes clicked
-   *****************************************************/
-  let ls_amen = [];
-  $('input[type=checkbox]').change (function () {
-    let name = $(this).attr('data-name');
-      if ($(this).is(':checked')) {
-	ls_amen.push(name);
-      } else {
-	ls_amen = ls_amen.filter(amen => amen !== name);
-      }
-    $('.amenities h4').text(ls_amen.join(', '));
-  });
-
-  /*******************************************************
-    display red circle on top right of page if status ok
-   *******************************************************/
-  $.ajax({
-    type: 'GET',
-    url: 'http://0.0.0.0:5001/api/v1/status/',
-    dataType: 'json',
-    success: function (data) {
-      if (data.status === 'OK') {
-	$('#api_status').addClass('available');
-      } else {
-	$('#api_status').removeClass('available');
-      }
-    }
-  });
-
-});
->>>>>>> 7935defc74ddedb01422bd63848c4da05a9eba93
